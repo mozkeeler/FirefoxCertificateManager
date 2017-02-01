647e0711b7623e3b37e9d2cb53a597c7ec89b656
David Keeler <mozkeeler@gmail.com>
2017-01-31 16:49:25 -0800

    update builtin CA information as of 31 January 2017

--- SalesForce Generator/generator.py
+++ SalesForce Generator/generator.py
@@ -3,27 +3,43 @@ import json
 import ast
 from datetime import datetime
 
-pendingCertsPath = 'Pending CA Certificate Requests - Sheet1.csv'
 builtInCertsPath = 'BuiltInCAs.csv'
 builtInCertsFile = file(builtInCertsPath, 'r')
 builtInCertsOutput = 'SalesForceData.js'
 
-def getBuiltInCerts():
-	output = open(builtInCertsOutput, 'w')
-	reader = csv.reader(builtInCertsFile)
-	salesforceJson = {}
+def findIndexOf(string, columns):
+    for i, item in enumerate(columns):
+        if item == string:
+            return i
+    raise Error("could not find column '%s'" % string)
 
-	for row in reader:
-		cert = {}
-		cert['trustBits'] = row[9]
-		cert['auditDate'] = row[25].strip()
-		cert['geographicFocus'] = row[17]
-		cert['owner'] = row[0]
-		if not salesforceJson.has_key(row[1]):
-			salesforceJson[row[1]] = cert	
-		else:
-			salesforceJson[row[1]] = getMostRecentCert(salesforceJson[row[1]], cert)
-	output.write('var certManagerJsonText = '+ json.dumps(salesforceJson, indent=2)+ '''
+def getBuiltInCerts():
+    output = open(builtInCertsOutput, 'w')
+    reader = csv.reader(builtInCertsFile)
+    salesforceJson = {}
+
+    rows = []
+    for row in reader:
+        rows.append(row)
+    columns = rows[0]
+    trustBitsIndex = findIndexOf("Trust Bits", columns)
+    auditDateIndex = findIndexOf("Standard Audit Statement Dt", columns)
+    geographicFocusIndex = findIndexOf("Geographic Focus", columns)
+    ownerIndex = findIndexOf("Owner", columns)
+    issuerOrganizationIndex = findIndexOf("Certificate Issuer Organization", columns)
+
+    for row in rows[1:]:
+        cert = {}
+        cert['trustBits'] = row[trustBitsIndex]
+        cert['auditDate'] = row[auditDateIndex].strip()
+        cert['geographicFocus'] = row[geographicFocusIndex]
+        cert['owner'] = row[ownerIndex]
+        issuerOrganization = row[issuerOrganizationIndex]
+        if not salesforceJson.has_key(issuerOrganization):
+            salesforceJson[issuerOrganization] = cert
+        else:
+            salesforceJson[issuerOrganization] = getMostRecentCert(salesforceJson[issuerOrganization], cert)
+    output.write('var certManagerJsonText = ' + json.dumps(salesforceJson, indent=2, separators=(',', ': ')) + '''
 var certManagerJson = certManagerJsonText;
 
 function getJSON() {
@@ -31,45 +47,44 @@ function getJSON() {
 }
 
 exports.getJSON = getJSON;
-		''')
+''')
 
 
 def getMostRecentCert(cert1, cert2):
-	result = {}
-	date1 = None
-	date2 = None
+    result = {}
+    date1 = None
+    date2 = None
 
-	if not cert1['auditDate'] == '':
-		date1 = datetime.strptime(cert1['auditDate'], '%Y.%m.%d')
+    if not cert1['auditDate'] == '':
+        date1 = datetime.strptime(cert1['auditDate'], '%Y.%m.%d')
 
-	if not cert2['auditDate'] == '':
-		date2 = datetime.strptime(cert2['auditDate'], '%Y.%m.%d')
+    if not cert2['auditDate'] == '':
+        date2 = datetime.strptime(cert2['auditDate'], '%Y.%m.%d')
 
-	bits1 = [x.strip() for x in cert1['trustBits'].split(';')]
-	bits2 = [x.strip() for x in cert2['trustBits'].split(';')]
-	geo = cert1['geographicFocus']
-	bitsUnion = union(bits1, bits2)
+    bits1 = [x.strip() for x in cert1['trustBits'].split(';')]
+    bits2 = [x.strip() for x in cert2['trustBits'].split(';')]
+    geo = cert1['geographicFocus']
+    bitsUnion = union(bits1, bits2)
 
-	if date1 == None and date2 == None:
-		result['auditDate'] = ''
-	elif date1 == None:
-		result['auditDate'] = date2.strftime('%Y.%m.%d')
-	elif date2 == None:
-		result['auditDate'] = date1.strftime('%Y.%m.%d')	
-	else:	
-		result['auditDate'] = max(date1, date2).strftime('%Y.%m.%d')
-	result['trustBits'] = '; '.join(bitsUnion)
-	result['geographicFocus'] = geo
-	result['owner'] = cert2['owner']
+    if date1 == None and date2 == None:
+        result['auditDate'] = ''
+    elif date1 == None:
+        result['auditDate'] = date2.strftime('%Y.%m.%d')
+    elif date2 == None:
+        result['auditDate'] = date1.strftime('%Y.%m.%d')
+    else:
+        result['auditDate'] = max(date1, date2).strftime('%Y.%m.%d')
+    result['trustBits'] = '; '.join(bitsUnion)
+    result['geographicFocus'] = geo
+    result['owner'] = cert2['owner']
 
-	return result
+    return result
 
 def union(a, b):
-	return list(set(a) | set(b))
+    return list(set(a) | set(b))
 
 def main():
-	# getPendingCerts()
-	getBuiltInCerts()
+    getBuiltInCerts()
 
-if __name__ == '__main__': 
-	main()
+if __name__ == '__main__':
+    main()
